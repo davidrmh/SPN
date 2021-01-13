7fe9c52ee15a9c79838e3481b3584c2dcb121c6e
davidrmh <davidricardo888@hotmail.com>
2021-01-13 17:33:31 +0100

    Add function reachable_x

--- src/structure_learning.jl
+++ src/structure_learning.jl
@@ -129,3 +129,33 @@ function copysubspn(root::Union{SumNode, ProductNode})
     _register_copy!(subspn);
     return subspn
 end
+
+"""
+    reachable_x(node, varname)
+
+Get the distribution (indicator) nodes related to `varname` that can be reached from `node`.
+This function corresponds to the function I_{x}(N) in the paper
+`Learning Selective Sum-Product Networks by Peharz, R. et al`.
+
+# Arguments
+- `node::AbstractNode` Node from which the search is started.
+- `varname::Symbol` Variable of interest.
+
+If we have a variable `X1` that can take on `K` distinct values, `X1_1, ... X1_K`
+then varname is `:X1`.
+
+Return an array of symbols corresponding to the names of each reachable
+distribution (indicator) node.
+"""
+function reachable_x(node::AbstractNode, varname::Symbol)
+    #Get all variables that can be reached from `node`
+    variables = variablenames(node)
+
+    #keep only the ones related to `varname`
+    str_target = string(varname)
+    I_X = []
+    for v in variables
+        occursin(str_target, string(v)) ? push!(I_X, v) : nothing
+    end
+    I_X
+end
