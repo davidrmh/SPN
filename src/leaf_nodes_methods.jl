5323087b52e65228b488f10644b4f8bc5d2be31d
davidrmh <davidricardo888@hotmail.com>
2021-01-21 18:55:26 +0100

    Modify pdf function to receive an argument of type LeafNode

--- src/leaf_nodes_methods.jl
+++ src/leaf_nodes_methods.jl
@@ -1,14 +1,7 @@
 #========================================
-Methods for Distribution nodes
+Methods for Leaf nodes
 ========================================#
-function pdf(d::DistributionNode, x::Union{Real, AbstractArray, NamedTuple})
+function pdf(d::LeafNode, x::Union{Real, AbstractArray, NamedTuple})
     value = isa(x, NamedTuple) ? x[d.varname] : x
     Distributions.pdf(d.distribution, value)
-end
-
-#========================================
-Methods for Indicator nodes
-========================================#
-function pdf(d::IndicatorNode, x::Union{Real, AbstractArray, NamedTuple})
-    x[d.varname]
 end
