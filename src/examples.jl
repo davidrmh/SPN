6cac608be61208862883f769261f20fa24ef6408
davidrmh <davidricardo888@hotmail.com>
2021-01-27 20:31:46 +0100

    Fix typo in function name

--- src/examples.jl
+++ src/examples.jl
@@ -45,7 +45,7 @@ function naivebayesmixture(weights = [[0.5, 0.2, 0.3], [0.6, 0.4], [0.9, 0.1], [
     s1
 end
 
-function independet(num_var = 3)
+function independent(num_var = 3)
     #root node
     root = ProductNode([], [undef])
 
