e62a3beaed3caaa78aa5ed650a06e31158df8cd7
davidrmh <davidricardo888@hotmail.com>
2021-03-09 13:24:30 +0100

    Add workflow

--- src/workflows.jl
+++ src/workflows.jl
@@ -4,7 +4,7 @@ Workflow for MLE in selective SPNs (Fixed architecture)
 ======================================================#
 #True data generator process
 regsel = regularselective();
-nsamples = 100_000;
+nsamples = 10_000;
 data = sample(regsel, nsamples);
 
 _reset_counter!();
@@ -18,3 +18,18 @@ calctrees = getcalctrees(model, dictval);
 #Update model's parameters
 selective_mle!(model, calctrees);
 dotfile(model, "dotfiles\\test.gv");
+#======================================================
+Workflow for calculating the partial derivatives of
+the log-likelihood function with respect to the weights
+of each sum node
+======================================================#
+_reset_counter!();
+model = regularselective();
+nsamples = 1;
+modeldata = sample(model, nsamples);
+modelparam = getparameters(model, false)[1];
+logpdfmem = Dict();
+logpdf!(model, modeldata, modelparam, logpdfmem);
+#nodes derivatives
+nodesderiv = nodesderivatives(model, logpdfmem, true);
+weights_deriv = weights_loglike_deriv(model, logpdfmem, nodesderiv);
